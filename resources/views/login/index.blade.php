554f262e54a843c5d1caca715138d21c3d6c2d2c
Sander Dechering <sander.dechering@gmail.com>
2018-10-02 13:07:12 +0200

    test

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,1 +1,11 @@
-login
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col s4 offset-s4 panel">
+                panel
+            </div>
+        </div>
+    </div>
+@endsection
